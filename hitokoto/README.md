a04c19728da5802379abf26e048d00413fe324c2
ImJingLan <imjinglan@126.com>
2021-01-28 10:00:53 +0800

    Lighter
    Create hitokoto/README.md file
    del 'n' in index.php

---
+++ hitokoto/README.md
@@ -0,0 +1,17 @@
+# [**一言 Hitokoto**](https://hitokoto.cn/)
+
+## 简介
+
+为HTML静态网页提供实时更新的一言语句的script脚本
+
+## 原理
+
+1.获取[一言纯文本](https://v1.hitokoto.cn/?encode=text)
+
+2.输出 script 脚本 document.write('获取的语句');
+
+3.HTML加载脚本，将文字写入网页
+
+## 语言
+
+PHP : 100%
--- hitokoto/index.php
+++ hitokoto/index.php
@@ -1,6 +1,2 @@
-<?php
-$encode=1;
-$text= file_get_contents('https://v1.hitokoto.cn?encode=text');
-if($encode==1)
-    echo "document.write('".$text."');n";
+<?php$text= file_get_contents('https://v1.hitokoto.cn?encode=text');echo "document.write('".$text."');";
 ?>
